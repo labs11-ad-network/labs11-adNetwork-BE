11efff40a3c84599038ce5e4097b675e22341f6c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-20 16:47:51 -0400

    Oauth and native login integration

--- routes/users/index.js
+++ routes/users/index.js
@@ -8,21 +8,29 @@ route.get("/", (req, res) => {
 });
 
 route.post("/register", async (req, res) => {
-  const { first_name, last_name, email, password, phone, acct_type } = req.body;
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    phone,
+    acct_type,
+    image_url,
+    oauth_token
+  } = req.body;
 
   try {
-    console.log(req.body);
-    if (
-      !first_name ||
-      !last_name ||
-      !email ||
-      !password ||
-      !phone ||
-      !acct_type
-    )
+    if (!first_name || !last_name || !email || !phone || !acct_type)
       return res.status(422).json({ message: "All fields required" });
 
-    const hash = bcrypt.hashSync(password, 14);
+    if (!password && !oauth_token)
+      return res
+        .status(500)
+        .json({
+          message:
+            "you must sign up using a password or some other social network"
+        });
+    const hash = password ? bcrypt.hashSync(password, 14) : null;
 
     const user = await models.findBy("users", { email });
 
@@ -32,9 +40,11 @@ route.post("/register", async (req, res) => {
       first_name,
       last_name,
       email,
-      password: hash,
+      password: oauth_token ? null : hash,
       phone,
-      acct_type
+      acct_type,
+      image_url,
+      oauth_token
     });
 
     const newUser = await models.findBy("users", { id });
@@ -47,12 +57,16 @@ route.post("/register", async (req, res) => {
 });
 
 route.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, oauth_token } = req.body;
   try {
-    if (!email || !password)
-      return res.status(422).json({ message: "All fields required" });
+    if (!email) return res.status(422).json({ message: "All fields required" });
 
     const user = await models.findBy("users", { email });
+
+    if (oauth_token) {
+      const oauth_user = await models.findBy("users", { oauth_token });
+      if (oauth_user) return res.json(oauth_user);
+    }
 
     if (!user) return res.status(404).json({ message: "User does not exist" });
 
