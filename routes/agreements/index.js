c5eccab5be9ec4e17e7d7d93fc7c56cf6f9483af
mcferrenm <mcferrenmax@gmail.com>
2019-03-21 13:48:29 -0400

    Agreements POST route

--- routes/agreements/index.js
+++ routes/agreements/index.js
@@ -16,14 +16,44 @@ route.get("/", async (req, res) => {
   }
 });
 
-route.get('/:id', async (req, res) => {
+route.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const agreement = await models.findBy('agreements', { id });
+    const agreement = await models.findBy("agreements", { id });
     if (agreement) {
       res.status(200).json(agreement);
     } else {
-      res.status(404).json({ message: 'There was no agreement found at that ID.'})
+      res
+        .status(404)
+        .json({ message: "There was no agreement found at that ID." });
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+
+// Without authentication. When we add that we will refactor based on 
+// whether req.decoded.id is affiliate_id or advertiser_id
+route.post("/", async (req, res) => {
+  if (
+    !(
+      req.body.hasOwnProperty("offer_id") &&
+      req.body.hasOwnProperty("affiliate_id") &&
+      req.body.hasOwnProperty("advertiser_id")
+    )
+  ) {
+    res.status(400).json({ message: "Required information is missing." });
+  }
+  try {
+    const [id] = await models.add("agreements", req.body);
+    if (id) {
+      const agreement = await models.findBy("agreements", { id });
+      res.status(201).json(agreement);
+    } else {
+      res
+        .status(404)
+        .json({ message: "There was an issue adding agreement at that ID." });
     }
   } catch ({ message }) {
     res.status(500).json({ message });
--- routes/agreements/index.spec.js
+++ routes/agreements/index.spec.js
@@ -2,6 +2,10 @@ const request = require("supertest");
 const server = require("../../api/server");
 const db = require("../../data/dbConfig");
 
+beforeAll(() => {
+  return db("agreements").truncate();
+});
+
 describe("Agreement routes", () => {
   describe("Agreement CRUD routes", () => {
     describe("Agreement GET routes", () => {
@@ -14,6 +18,18 @@ describe("Agreement routes", () => {
         return request(server)
           .get("/api/agreements/0981238")
           .then(res => expect(res.status).toBe(404));
+      });
+    });
+    describe("Agreement POST route", () => {
+      it("should return 201 if the agreement is added successfully", () => {
+        return request(server)
+          .post("/api/agreements")
+          .send({
+            offer_id: 1,
+            affiliate_id: 1,
+            advertiser_id: 3
+          })
+          .then(res => expect(res.status).toBe(201));
       });
     });
   });
