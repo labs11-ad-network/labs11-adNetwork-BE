75bea04c0cd702859a0e7f2546fac5266ea7cf8b
mcferrenm <mcferrenmax@gmail.com>
2019-03-21 12:14:06 -0400

    Agreements GET routes

--- routes/agreements/index.js
+++ routes/agreements/index.js
@@ -2,8 +2,32 @@ const route = require("express").Router();
 const models = require("../../common/helpers");
 
 route.get("/", async (req, res) => {
-  const agreements = await models.get("agreements");
-  res.status(200).json(agreements);
+  try {
+    const agreements = await models.get("agreements");
+    if (agreements) {
+      res.status(200).json(agreements);
+    } else {
+      res
+        .status(404)
+        .json({ message: "There was an issue retrieving the offers." });
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+route.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const agreement = await models.findBy('agreements', { id });
+    if (agreement) {
+      res.status(200).json(agreement);
+    } else {
+      res.status(404).json({ message: 'There was no agreement found at that ID.'})
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
 });
 
 module.exports = route;
--- routes/agreements/index.spec.js
+++ routes/agreements/index.spec.js
@@ -2,10 +2,19 @@ const request = require("supertest");
 const server = require("../../api/server");
 const db = require("../../data/dbConfig");
 
-describe('Agreements route', () => {
-  it('should return 200 if the request succeeds', () => {
-    return request(server)
-      .get("/api/agreements")
-      .then(res => expect(res.status).toBe(200));
+describe("Agreement routes", () => {
+  describe("Agreement CRUD routes", () => {
+    describe("Agreement GET routes", () => {
+      it("should return 200 if the request succeeds", () => {
+        return request(server)
+          .get("/api/agreements")
+          .then(res => expect(res.status).toBe(200));
+      });
+      it("should return 404 if agreement is not found", () => {
+        return request(server)
+          .get("/api/agreements/0981238")
+          .then(res => expect(res.status).toBe(404));
+      });
+    });
   });
 });
