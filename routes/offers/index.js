369596ed7dc709c32286e542bd3f7d790135f038
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2019-03-20 21:29:40 -0500

    Add files via upload

--- routes/offers/index.js
+++ routes/offers/index.js
@@ -1,42 +1,85 @@
-const route = require("express").Router();
-const models = require("../../common/helpers");
+const route = require("express").Router()
+const models = require("../../common/helpers")
 
 route.get("/", async (req, res) => {
   try {
-    const offers = await models.get("offers");
-    res.json(offers);
-  } catch (error) {
-    res.status(500).json(error);
+    const offers = await models.get('offers')
+    if(offers) {
+      res.status(200).json(offers)
+    } else {
+      res.status(404).json({ message: 'There was an issue retrieving the offers.' })
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message })
   }
 });
 
-route.get("/:id", async (req, res) => {
+route.get('/:id', async (req, res) => {
+  const id = req.params.id
   try {
-    const { id } = req.params;
-    const offer = await models.findBy("offers", { id });
-
-    if (!offer) return res.status(404).json({ message: "Offer not found" });
-
-    res.json(offer);
-  } catch (error) {
-    res.status(500).json(error);
+    const offer = await models.findBy('offers', { id });
+    if(offer) {
+      res.status(200).json(offer)
+    } else {
+      res.status(404).json({ message: 'There was no offer found at that ID.' })
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message });
   }
 });
 
-// route.post('/', async (req, res) => {
-//   const {
-//     budget,
-//     name,
-//     description,
-//     category,
-//     currency,
-//     status
-//   }
-//   try {
+route.post('/', async (req, res) => {
+  // Make sure to stop any attempts to create any info with IDs or timestamps
+  if( !(req.body.hasOwnProperty('budget') &&
+        req.body.hasOwnProperty('name') &&
+        req.body.hasOwnProperty('description') &&
+        req.body.hasOwnProperty('category') &&
+        req.body.hasOwnProperty('currency') &&
+        req.body.hasOwnProperty('status')) ) {
+          res.status(400).json({ message: 'Required information is missing.'})
+    }
 
-//   } catch (error) {
+  try {
+    const [ id ] = await models.add('offers', { ...req.body })
+    if(id) {
+      const offer = await models.findBy('offers', { id })
+      res.status(201).json(offer)
+    } else {
+      res.status(404).json({ message: 'There was an issue adding user at that ID.' })
+    }
+  } catch({ message }) {
+    res.status(500).json({ message })
+  }
+})
 
-//   }
-// })
+route.put('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const success = await models.update('offers', id, { ...req.body })
+    if(success) {
+      const offer = await models.findBy('offers', { id })
+      res.status(200).json({ offer, message: 'Offer successfully edited.' })
+    } else {
+      res.status(404).json({ message: 'There was an issue editing the offer at that ID.' })
+    }
+  } catch({ message }) {
+    res.status(500).json({ message })
+  }
+})
+
+
+route.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const success = await models.remove('offers', id)
+    if(success) {
+      res.status(200).json({ message: 'User successfully deleted.' })
+    } else {
+      res.status(404).json({ message: 'There was an issue deleting the user at that ID.' })
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message })
+  }
+})
 
 module.exports = route;
