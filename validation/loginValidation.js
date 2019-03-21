777c84a6ec32a2ec94bb98b88ab6e54269c3341c
john2796 <jbmiranda22796@gmail.com>
2019-03-21 00:11:28 -0700

    added comments on validation as well

--- validation/loginValidation.js
+++ validation/loginValidation.js
@@ -3,15 +3,16 @@ const isEmpty = require("./isEmpty");
 
 module.exports = function validateLogin(data) {
   let errors = {};
-
   data.email = !isEmpty(data.email) ? data.email : "";
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  }
 
+  // ----------------- has to be a valid email ----------------------
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  }
+  // ----------------- fields are required ----------------------
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
 
   return {
