e30b1977e06e0ac14ecd36441c7d9b20ebd582f5
john2796 <jbmiranda22796@gmail.com>
2019-03-22 13:34:00 -0700

    setting up image upload

--- routes/mockups/index.js
+++ routes/mockups/index.js
@@ -2,6 +2,20 @@ const route = require("express").Router();
 const models = require("../../common/helpers");
 const { authenticate } = require("../../common/authentication");
 
+const cloudinary = require("cloudinary");
+const multipart = require("connect-multiparty")();
+
+//console.log(process.env.CD_KEY, process.env.CD_SECRET)
+cloudinary.config({
+  cloud_name: "lambda-school",
+  api_key: process.env.CD_KEY,
+  api_secret: process.env.CD_SECRET
+})
+console.log()
+
+
+
+
 route.get("/", authenticate, async (req, res) => {
   try {
     const ads = await models.get("ads");
@@ -14,17 +28,52 @@ route.get("/", authenticate, async (req, res) => {
 // @route    GET /api/ads
 // @desc     post ads
 // @Access   Private
-route.post("/", authenticate, async (req, res) => {
-  const user_id = req.decoded.id;
+// route.post("/", authenticate, multipart, async (req, res) => {
+//   const user_id = req.decoded.id;
+//   // try {
+//   //   const [newAd] = await models.add("ads", { ...req.body, user_id });
+//   //   if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
+//   //   const ad = await models.findBy("ads", { id: newAd });
+//   //   res.json(ad);
+//   // } catch ({ message }) {
+//   //   res.status(500).json({ message });
+//   // }
+//   console.log('req.files', req.files);
+//   cloudinary.v2.uploader.upload(req.files, async function (
+//     error,
+//     result
+//   ) {
+//     if (error) {
+//       res.status(500).json({ message: "Upload failed" });
+//     } else {
+//       try {
+//         res.json({ result })
+
+//       } catch ({ message }) {
+//         res.status(500).json({ message });
+//       }
+//     }
+//   });
+// });
+
+// @route    GET api/ads/upload
+// @desc     testing upload 
+// @Access   Public
+route.post('/upload', multipart, async (req, res) => {
   try {
-    const [newAd] = await models.add("ads", { ...req.body, user_id });
-    if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
-    const ad = await models.findBy("ads", { id: newAd });
-    res.json(ad);
-  } catch ({ message }) {
-    res.status(500).json({ message });
+    let filename = req.files.dataFile.path;
+    console.log("--------------- filename ---------------", filename)
+    res.send('test')
+
+
+  } catch (err) {
+    res.status(500).json(err)
   }
-});
+})
+
+
+
+
 
 route.delete("/:id", authenticate, async (req, res) => {
   const { id } = req.params;
