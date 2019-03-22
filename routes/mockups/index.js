503dcecf5dfc682d1b65b363df66b9834f4eb34a
john2796 <jbmiranda22796@gmail.com>
2019-03-21 20:47:06 -0700

    added cloudinary depenedencies

--- routes/mockups/index.js
+++ routes/mockups/index.js
@@ -1,14 +1,6 @@
 const route = require("express").Router();
 const models = require("../../common/helpers");
 const { authenticate } = require("../../common/authentication");
-const cloudinary = require('cloudinary');
-const multipart = require('connect-multiparty')();
-
-cloudinary.config({
-  cloud_name: 'lambda-school',
-  api_key: process.env.CD_KEY,
-  api_secret: process.env.CD_SECRET,
-})
 
 
 route.get("/", authenticate, async (req, res) => {
