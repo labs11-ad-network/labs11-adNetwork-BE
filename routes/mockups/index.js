d0108d5133754322379cba1197aeb274e9e56ee5
john2796 <jbmiranda22796@gmail.com>
2019-03-25 08:43:57 -0700

    removed console logs

--- routes/mockups/index.js
+++ routes/mockups/index.js
@@ -1,20 +1,15 @@
 const route = require("express").Router();
 const models = require("../../common/helpers");
 const { authenticate } = require("../../common/authentication");
-
-const cloudinary = require("cloudinary");
+const db = require('../../data/dbConfig')
+const cloudinary = require('cloudinary');
 const multipart = require("connect-multiparty")();
 
-//console.log(process.env.CD_KEY, process.env.CD_SECRET)
 cloudinary.config({
-  cloud_name: "lambda-school",
-  api_key: process.env.CD_KEY,
-  api_secret: process.env.CD_SECRET
-})
-console.log()
-
-
-
+  cloud_name: 'dxvyzmvhi',
+  api_key: '672796748434519',
+  api_secret: 'Jf7IESazEon7JKlD9dd8fkMgESk'
+});
 
 route.get("/", authenticate, async (req, res) => {
   try {
@@ -25,55 +20,42 @@ route.get("/", authenticate, async (req, res) => {
   }
 });
 
-// @route    GET /api/ads
-// @desc     post ads
-// @Access   Private
-// route.post("/", authenticate, multipart, async (req, res) => {
-//   const user_id = req.decoded.id;
-//   // try {
-//   //   const [newAd] = await models.add("ads", { ...req.body, user_id });
-//   //   if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
-//   //   const ad = await models.findBy("ads", { id: newAd });
-//   //   res.json(ad);
-//   // } catch ({ message }) {
-//   //   res.status(500).json({ message });
-//   // }
-//   console.log('req.files', req.files);
-//   cloudinary.v2.uploader.upload(req.files, async function (
-//     error,
-//     result
-//   ) {
-//     if (error) {
-//       res.status(500).json({ message: "Upload failed" });
-//     } else {
-//       try {
-//         res.json({ result })
+// route.post('/images', multipart, async (req, res) => {
+//     console.log(req.files);
+//   cloudinary.v2.uploader.upload(
+//   req.files.image.path,
+//    (error, result) => {
+//      if(error) return res.json({message: error})
+//      res.json(result)
+//    })
+// })
 
-//       } catch ({ message }) {
-//         res.status(500).json({ message });
-//       }
-//     }
-//   });
-// });
+route.post("/", authenticate, multipart, async (req, res) => {
+  const user_id = req.decoded.id;
 
-// @route    GET api/ads/upload
-// @desc     testing upload 
-// @Access   Public
-route.post('/upload', multipart, async (req, res) => {
-  try {
-    let filename = req.files.dataFile.path;
-    console.log("--------------- filename ---------------", filename)
-    res.send('test')
+  cloudinary.v2.uploader.upload(
+    req.files.image.path,
+    async (error, result) => {
+      if (error) return res.status(500).json({ message: error })
+      try {
+        const [newAd] = await models.add("ads", { ...req.body, back_img: result.secure_url, user_id });
+        if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
+        const ad = await models.findBy("ads", { id: newAd });
+        res.json(ad);
+      } catch ({ message }) {
+        res.status(500).json({ message })
+      }
+    })
 
-
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-
-
-
-
+  // try {
+  //   const [newAd] = await models.add("ads", { ...req.body, user_id });
+  //   if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
+  //   const ad = await models.findBy("ads", { id: newAd });
+  //   res.json(ad);
+  // } catch ({ message }) {
+  //   res.status(500).json({ message });
+  // }
+});
 
 route.delete("/:id", authenticate, async (req, res) => {
   const { id } = req.params;
@@ -110,11 +92,26 @@ route.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
+    // const ad = await db.select('a.*', 'ag.*').from('ads as a').join('agreements as ag', 'ag.offer_id', 'a.offer_id')
     const ad = await models.findBy("ads", { id });
+    console.log({ ad })
     if (!ad) return res.status(404).json({ message: "No ads found" });
     res.json(ad);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+route.get("/offers/:id", authenticate, async (req, res) => {
+  const user_id = req.decoded.id;
+  const offer_id = req.params.id;
+
+  try {
+    const ads = await models.findAllBy("ads", { user_id, offer_id });
+    if (!ads.length) return res.status(404).json({ message: "No Ads found" });
+    res.json(ads);
+  } catch ({ message }) {
+    res.status(500).json({ message });
   }
 });
 
