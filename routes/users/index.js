1f589365917fcbb9171f9937a7b75873dd0ea714
john2796 <jbmiranda22796@gmail.com>
2019-03-27 10:24:42 -0700

    resolve incoming changes

--- routes/users/index.js
+++ routes/users/index.js
@@ -62,12 +62,19 @@ route.put("/:id", authenticate, async (req, res) => {
   }
 });
 
-route.delete("/:id", authenticate, async (req, res) => {
-  const id = req.params.id;
+
+
+// @route    GET api/user
+// @desc     delete user account
+// @Access   Public
+route.delete("/", authenticate, async (req, res) => {
+
+  const { id } = req.decoded
   try {
     if (id !== req.decoded.id) {
       return res.status(500).json({ message: "You cannot delete someone account" })
     }
+
     const success = await models.remove("users", id);
     if (success) {
       res.status(200).json({ message: "User deleted successfully." });
