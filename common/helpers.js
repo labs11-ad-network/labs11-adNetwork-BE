63766253a73d716ba9d3cdb952e3c0fb89d2cc7b
john2796 <jbmiranda22796@gmail.com>
2019-03-26 21:40:38 -0700

    accepted incoming changes

--- common/helpers.js
+++ common/helpers.js
@@ -29,21 +29,98 @@ const queryByDate = (tbl, started_at, ended_at) =>
     .where("created_at", ">=", started_at)
     .where("created_at", "<", ended_at);
 
-const analyticsWithPricing = id => db.select('an.*', 'o.price_per_click', 'o.price_per_impression')
-                                      .from('analytics as an')
-                                      .join('agreements as ag', 'ag.affiliate_id', id )
-                                      .join('offers as o', 'ag.offer_id', 'o.id')
+const analyticsWithPricing = id =>
+  db("agreements as ag")
+    .join("analytics as an", "an.agreement_id", "ag.id")
+    .join("offers as o", "o.id", "ag.offer_id")
+    .where("ag.affiliate_id", id)
+    .select("an.*", "o.price_per_click", "o.price_per_impression");
 
-const analyticsWithPricingAdvertiser = id => db.select('an.*', 'o.price_per_click', 'o.price_per_impression')
-                                                .from('analytics as an')
-                                                .join('agreements as ag', 'ag.id', 'an.agreement_id')
-                                                .join('offers as o', 'ag.offer_id', 'o.id')
-                                                .where('user_id', id)
+const actionCountAffiliate = (id, filter) =>
+  db
+    .select("an.*", "o.price_per_click", "o.price_per_impression")
+    .from("analytics as an")
+    .join("agreements as ag", "ag.affiliate_id", id)
+    .join("offers as o", "ag.offer_id", "o.id")
+    .where("action", filter);
 
-const getAdvertiserEmail = id => db.select('u.*')
-                                    .from('agreements as ag')
-                                    .join('offers as o', 'o.id', id)
-                                    .join('users as u', 'o.user_id', 'u.id')
+const analyticsWithPricingAdvertiser = id =>
+  db
+    .select("an.*", "o.price_per_click", "o.price_per_impression")
+    .from("analytics as an")
+    .join("agreements as ag", "ag.id", "an.agreement_id")
+    .join("offers as o", "ag.offer_id", "o.id")
+    .where("user_id", id);
+
+const getAdvertiserEmail = id =>
+  db
+    .select("u.*")
+    .from("agreements as ag")
+    .join("offers as o", "o.id", id)
+    .join("users as u", "o.user_id", "u.id");
+
+const actionCount = (filter, id) =>
+  db
+    .select()
+    .from("analytics as an")
+    .join("agreements as ag", "ag.id", "an.agreement_id")
+    .join("offers as o", "ag.offer_id", "o.id")
+    .where("user_id", id)
+    .andWhere("action", filter);
+
+const browserCountAdvertisers = (filter, id) =>
+  db
+    .select()
+    .from("analytics as an")
+    .join("agreements as ag", "ag.id", "an.agreement_id")
+    .join("offers as o", "ag.offer_id", "o.id")
+    .where("user_id", id)
+    .andWhere("browser", filter);
+
+const browserCountAffiliates = (filter, id) =>
+  db
+    .select()
+    .from("analytics as an")
+    .join("agreements as ag", "ag.id", "an.agreement_id")
+    .join("offers as o", "ag.affiliate_id", id)
+    // .where("user_id", id)
+    .where("browser", filter);
+
+const analyticsPerOfferWithPricing = (filter, id, offer_id) =>
+  db("analytics as an")
+    .join("agreements as ag", "an.agreement_id", "ag.id")
+    .join("offers as o", "o.id", "ag.offer_id")
+    .where("offer_id", offer_id)
+    .andWhere("affiliate_id", id)
+    .andWhere("action", filter)
+    .select("an.*", "o.price_per_click", "o.price_per_impression");
+
+const browserCountPerOfferAffiliates = (filter, id, agreement_id) =>
+  db("analytics as an")
+    .join("agreements as ag", "an.agreement_id", "ag.id")
+    .join("offers as o", "o.id", "ag.offer_id")
+    .where("agreement_id", agreement_id)
+    .andWhere("affiliate_id", id)
+    .andWhere("browser", filter)
+    .select("an.*", "o.price_per_click", "o.price_per_impression");
+
+const analyticsPerOfferAdvertisers = (filter, id, offer_id) =>
+  db("analytics as an")
+    .join("agreements as ag", "an.agreement_id", "ag.id")
+    .join("offers as o", "o.id", "ag.offer_id")
+    .where("offer_id", offer_id)
+    .andWhere("user_id", id)
+    .andWhere("action", filter)
+    .select("an.*", "o.price_per_click", "o.price_per_impression");
+
+const analyticsPerOfferAdvertisersBrowsers = (filter, id, offer_id) =>
+  db("analytics as an")
+    .join("agreements as ag", "an.agreement_id", "ag.id")
+    .join("offers as o", "o.id", "ag.offer_id")
+    .where("offer_id", offer_id)
+    .andWhere("user_id", id)
+    .andWhere("browser", filter)
+    .select("an.*", "o.price_per_click", "o.price_per_impression");
 module.exports = {
   get,
   findBy,
@@ -54,5 +131,13 @@ module.exports = {
   queryByDate,
   analyticsWithPricing,
   analyticsWithPricingAdvertiser,
-  getAdvertiserEmail
+  getAdvertiserEmail,
+  actionCount,
+  actionCountAffiliate,
+  browserCountAdvertisers,
+  browserCountAffiliates,
+  analyticsPerOfferWithPricing,
+  browserCountPerOfferAffiliates,
+  analyticsPerOfferAdvertisers,
+  analyticsPerOfferAdvertisersBrowsers
 };
