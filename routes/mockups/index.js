4ce3c41e27a4d6530213ecb79229e6b470d4cdbc
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-05 13:39:21 -0400

    Using cloudinary to save base64 images

--- routes/mockups/index.js
+++ routes/mockups/index.js
@@ -30,32 +30,35 @@ route.get("/", authenticate, async (req, res) => {
 route.post("/", authenticate, multipart, async (req, res) => {
   const user_id = req.decoded.id;
 
-  try {
-    const [success] = await models.add("ads", { ...req.body, user_id });
-
-    if (success) {
-      const ad = await models.findBy("ads", { id: success });
-
-      res.json(ad);
-    }
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-  // cloudinary.v2.uploader.upload(req.body.image, async (error, result) => {
-  //   if (error) return res.status(500).json({ message: error });
-  //   try {
-  //     const [newAd] = await models.add("ads", {
-  //       ...req.body,
-  //       back_img: result.secure_url,
-  //       user_id
-  //     });
-  //     if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
-  //     const ad = await models.findBy("ads", { id: newAd });
+  // try {
+  //   const [success] = await models.add("ads", { ...req.body, user_id });
+  //
+  //   if (success) {
+  //     const ad = await models.findBy("ads", { id: success });
+  //
   //     res.json(ad);
-  //   } catch ({ message }) {
-  //     res.status(500).json({ message });
+  //   } else {
+  //     res.status(400).json({ message: "Failed to create ad" });
   //   }
-  // });
+  // } catch ({ message }) {
+  //   res.status(500).json({ message: "Server Error" });
+  // }
+
+  cloudinary.v2.uploader.upload(req.body.image, async (error, result) => {
+    if (error) return res.status(500).json({ message: error });
+    try {
+      const [newAd] = await models.add("ads", {
+        ...req.body,
+        image: result.secure_url,
+        user_id
+      });
+      if (!newAd) return res.status(500).json({ message: "Failed to add ad" });
+      const ad = await models.findBy("ads", { id: newAd });
+      res.json(ad);
+    } catch ({ message }) {
+      res.status(500).json({ message });
+    }
+  });
 });
 
 // @route    GET /api/ads
