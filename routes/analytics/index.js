5fe4fd2d18b473213a83dec6c28100a59e7ee824
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-09 18:35:07 -0400

    Advertisers filtering

--- routes/analytics/index.js
+++ routes/analytics/index.js
@@ -421,20 +421,6 @@ route.get("/", authenticate, async (req, res) => {
           .thisMonthAffiliatesAll(affiliate_id, "conversion")
           .where("an.created_at", ">=", started_at)
           .andWhere("an.created_at", "<", ended_at);
-        const impressionsGrowthFiltered =
-          ((thisMonthsImpressionsFiltered.count -
-            lastMonthsImpressionsFiltered.count) /
-            thisMonthsImpressionsFiltered.count) *
-          100;
-        const clicksGrowthFiltered =
-          ((thisMonthClicksFiltered.count - lastMonthClicksFiltered.count) /
-            thisMonthClicksFiltered.count) *
-          100;
-        const conversionsGrowthFiltered =
-          ((thisMonthConversionsFiltered.count -
-            lastMonthConversionsFiltered.count) /
-            thisMonthConversionsFiltered.count) *
-          100;
         const citiesFiltered = await db("analytics as an")
           .join("agreements as ag", "ag.id", "an.agreement_id")
           .where("ag.affiliate_id", affiliate_id)
@@ -483,6 +469,21 @@ route.get("/", authenticate, async (req, res) => {
           .where("browser", "Other")
           .where("an.created_at", ">=", started_at)
           .andWhere("an.created_at", "<", ended_at);
+
+        const impressionsGrowthFiltered =
+          ((thisMonthsImpressionsFiltered.count -
+            lastMonthsImpressionsFiltered.count) /
+            thisMonthsImpressionsFiltered.count) *
+          100;
+        const clicksGrowthFiltered =
+          ((thisMonthClicksFiltered.count - lastMonthClicksFiltered.count) /
+            thisMonthClicksFiltered.count) *
+          100;
+        const conversionsGrowthFiltered =
+          ((thisMonthConversionsFiltered.count -
+            lastMonthConversionsFiltered.count) /
+            thisMonthConversionsFiltered.count) *
+          100;
 
         res.json({
           clicks: getAffiliateClicksFiltered,
@@ -564,105 +565,220 @@ route.get("/", authenticate, async (req, res) => {
         });
       }
     } else if (acct_type === "advertiser") {
-      const lastMonthsImpressions = await models.lastMonthAdvertiserAll(
-        affiliate_id,
-        "impression"
-      );
-      const thisMonthsImpressions = await models.thisMonthAdvertiserAll(
-        affiliate_id,
-        "impression"
-      );
-      const lastMonthClicks = await models.lastMonthAdvertiserAll(
-        affiliate_id,
-        "click"
-      );
-      const thisMonthClicks = await models.thisMonthAdvertiserAll(
-        affiliate_id,
-        "click"
-      );
+      if (started_at && ended_at) {
+        const lastMonthsImpressionsFiltered = await models
+          .lastMonthAdvertiserAll(affiliate_id, "impression")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const thisMonthsImpressionsFiltered = await models
+          .thisMonthAdvertiserAll(affiliate_id, "impression")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const lastMonthClicksFiltered = await models
+          .lastMonthAdvertiserAll(affiliate_id, "click")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const thisMonthClicksFiltered = await models
+          .thisMonthAdvertiserAll(affiliate_id, "click")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const lastMonthConversionsFiltered = await models
+          .lastMonthAdvertiserAll(affiliate_id, "conversion")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const thisMonthConversionsFiltered = await models
+          .thisMonthAdvertiserAll(affiliate_id, "conversion")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const citiesFiltered = await db("analytics as an")
+          .join("agreements as ag", "ag.id", "an.agreement_id")
+          .join("offers as o", "ag.offer_id", "o.id")
+          .where("o.user_id", affiliate_id)
+          .select("city", "longitude", "latitude")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at)
+          .count("* as num")
+          .groupBy("city", "longitude", "latitude");
+        const getAdvertisersClicksFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("action", "click")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const getAdvertisersImpressionsFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("action", "impression")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const getAdvertisersConversionFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("action", "conversion")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const chromeAnalyticsFiltered = await await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("browser", "Chrome")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const safariAnalyticsFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("browser", "Safari")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const edgeAnalyticsFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("browser", "Edge")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const firefoxAnalyticsFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("browser", "Firefox")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
+        const otherAnalyticsFiltered = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .where("browser", "Other")
+          .where("an.created_at", ">=", started_at)
+          .andWhere("an.created_at", "<", ended_at);
 
-      const lastMonthConversions = await models.lastMonthAdvertiserAll(
-        affiliate_id,
-        "conversion"
-      );
-      const thisMonthConversions = await models.thisMonthAdvertiserAll(
-        affiliate_id,
-        "conversion"
-      );
+        const impressionsGrowthFiltered =
+          ((thisMonthsImpressionsFiltered.count -
+            lastMonthsImpressionsFiltered.count) /
+            thisMonthsImpressionsFiltered.count) *
+          100;
+        const clicksGrowthFiltered =
+          ((thisMonthClicksFiltered.count - lastMonthClicksFiltered.count) /
+            thisMonthClicksFiltered.count) *
+          100;
+        const conversionsGrowthFiltered =
+          ((thisMonthConversionsFiltered.count -
+            lastMonthConversionsFiltered.count) /
+            thisMonthConversionsFiltered.count) *
+          100;
 
-      const impressionsGrowth =
-        ((thisMonthsImpressions.count - lastMonthsImpressions.count) /
-          thisMonthsImpressions.count) *
-        100;
+        res.json({
+          clicks: getAdvertisersClicksFiltered,
+          impressions: getAdvertisersImpressionsFiltered,
+          conversions: getAdvertisersConversionFiltered,
+          actionCount: {
+            impressions: Number(getAdvertisersImpressionsFiltered.length),
+            clicks: Number(getAdvertisersClicksFiltered.length),
+            conversions: Number(getAdvertisersConversionFiltered.length)
+          },
+          browserCount: {
+            chrome: chromeAnalyticsFiltered.length,
+            safari: safariAnalyticsFiltered.length,
+            edge: edgeAnalyticsFiltered.length,
+            firefox: firefoxAnalyticsFiltered.length,
+            other: otherAnalyticsFiltered.length
+          },
+          cities: citiesFiltered,
+          growth: {
+            clicks: clicksGrowthFiltered,
+            impressions: impressionsGrowthFiltered,
+            conversions: conversionsGrowthFiltered
+          }
+        });
+      } else {
+        const lastMonthsImpressions = await models.lastMonthAdvertiserAll(
+          affiliate_id,
+          "impression"
+        );
+        const thisMonthsImpressions = await models.thisMonthAdvertiserAll(
+          affiliate_id,
+          "impression"
+        );
+        const lastMonthClicks = await models.lastMonthAdvertiserAll(
+          affiliate_id,
+          "click"
+        );
+        const thisMonthClicks = await models.thisMonthAdvertiserAll(
+          affiliate_id,
+          "click"
+        );
 
-      const clicksGrowth =
-        ((thisMonthClicks.count - lastMonthClicks.count) /
-          thisMonthClicks.count) *
-        100;
+        const lastMonthConversions = await models.lastMonthAdvertiserAll(
+          affiliate_id,
+          "conversion"
+        );
+        const thisMonthConversions = await models.thisMonthAdvertiserAll(
+          affiliate_id,
+          "conversion"
+        );
 
-      const conversionsGrowth =
-        ((thisMonthConversions.count - lastMonthConversions.count) /
-          thisMonthConversions.count) *
-        100;
-      const cities = await db.raw(
-        `SELECT city, longitude, latitude,  count(*) as NUM FROM analytics JOIN agreements as ag ON ag.id = analytics.agreement_id JOIN offers as o ON ag.offer_id = o.id WHERE o.user_id = ${affiliate_id} GROUP BY city, longitude, latitude`
-      );
-      const analyticsForAdvertisersClicks = await models
-        .analyticsWithPricingAdvertiser(affiliate_id)
-        .andWhere("action", "click");
+        const impressionsGrowth =
+          ((thisMonthsImpressions.count - lastMonthsImpressions.count) /
+            thisMonthsImpressions.count) *
+          100;
 
-      const analyticsForAdvertisersImpressions = await models
-        .analyticsWithPricingAdvertiser(affiliate_id)
-        .andWhere("action", "impression");
+        const clicksGrowth =
+          ((thisMonthClicks.count - lastMonthClicks.count) /
+            thisMonthClicks.count) *
+          100;
 
-      const analyticsForAdvertisersConversions = await models
-        .analyticsWithPricingAdvertiser(affiliate_id)
-        .andWhere("action", "conversion");
+        const conversionsGrowth =
+          ((thisMonthConversions.count - lastMonthConversions.count) /
+            thisMonthConversions.count) *
+          100;
+        const cities = await db.raw(
+          `SELECT city, longitude, latitude,  count(*) as NUM FROM analytics JOIN agreements as ag ON ag.id = analytics.agreement_id JOIN offers as o ON ag.offer_id = o.id WHERE o.user_id = ${affiliate_id} GROUP BY city, longitude, latitude`
+        );
+        const analyticsForAdvertisersClicks = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .andWhere("action", "click");
 
-      const chromeAnalytics = await models.browserCountAdvertisers(
-        "Chrome",
-        affiliate_id
-      );
-      const safariAnalytics = await models.browserCountAdvertisers(
-        "Safari",
-        affiliate_id
-      );
-      const edgeAnalytics = await models.browserCountAdvertisers(
-        "Edge",
-        affiliate_id
-      );
-      const firefoxAnalytics = await models.browserCountAdvertisers(
-        "Firefox",
-        affiliate_id
-      );
-      const otherAnalytics = await models.browserCountAdvertisers(
-        "Other",
-        affiliate_id
-      );
+        const analyticsForAdvertisersImpressions = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .andWhere("action", "impression");
 
-      res.json({
-        clicks: analyticsForAdvertisersClicks,
-        impressions: analyticsForAdvertisersImpressions,
-        conversions: analyticsForAdvertisersConversions,
-        actionCount: {
-          impressions: Number(analyticsForAdvertisersImpressions.length),
-          clicks: Number(analyticsForAdvertisersClicks.length),
-          conversions: Number(analyticsForAdvertisersConversions.length)
-        },
-        browserCount: {
-          chrome: chromeAnalytics.length,
-          safari: safariAnalytics.length,
-          edge: edgeAnalytics.length,
-          firefox: firefoxAnalytics.length,
-          other: otherAnalytics.length
-        },
-        cities: cities.rows,
-        growth: {
-          clicks: clicksGrowth,
-          impressions: impressionsGrowth,
-          conversions: conversionsGrowth
-        }
-      });
+        const analyticsForAdvertisersConversions = await models
+          .analyticsWithPricingAdvertiser(affiliate_id)
+          .andWhere("action", "conversion");
+
+        const chromeAnalytics = await models.browserCountAdvertisers(
+          "Chrome",
+          affiliate_id
+        );
+        const safariAnalytics = await models.browserCountAdvertisers(
+          "Safari",
+          affiliate_id
+        );
+        const edgeAnalytics = await models.browserCountAdvertisers(
+          "Edge",
+          affiliate_id
+        );
+        const firefoxAnalytics = await models.browserCountAdvertisers(
+          "Firefox",
+          affiliate_id
+        );
+        const otherAnalytics = await models.browserCountAdvertisers(
+          "Other",
+          affiliate_id
+        );
+
+        res.json({
+          clicks: analyticsForAdvertisersClicks,
+          impressions: analyticsForAdvertisersImpressions,
+          conversions: analyticsForAdvertisersConversions,
+          actionCount: {
+            impressions: Number(analyticsForAdvertisersImpressions.length),
+            clicks: Number(analyticsForAdvertisersClicks.length),
+            conversions: Number(analyticsForAdvertisersConversions.length)
+          },
+          browserCount: {
+            chrome: chromeAnalytics.length,
+            safari: safariAnalytics.length,
+            edge: edgeAnalytics.length,
+            firefox: firefoxAnalytics.length,
+            other: otherAnalytics.length
+          },
+          cities: cities.rows,
+          growth: {
+            clicks: clicksGrowth,
+            impressions: impressionsGrowth,
+            conversions: conversionsGrowth
+          }
+        });
+      }
     }
   } catch ({ message }) {
     res.status(500).json({ message });
