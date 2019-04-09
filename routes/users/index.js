f76810abbae28490957c79a7c34b8bf924a855fe
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2019-04-09 11:30:51 -0500

    Used the wrong file from PR #92
    Same notes as closed PR #92

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,7 +3,6 @@ const multipart = require("connect-multiparty")();
 const route = require("express").Router();
 const models = require("../../common/helpers");
 const db = require("../../data/dbConfig");
-const stripe = require("stripe")(process.env.SECRET_KEY);
 
 const { authenticate } = require("../../common/authentication");
 
@@ -17,74 +16,51 @@ cloudinary.config({
 // @desc     Get current user
 // @Access   Private
 route.get("/", authenticate, async (req, res) => {
-  const { id, sub, email, acct_type } = req.decoded;
-  const _customer = await models.findBy("users", { id: req.decoded.id });
-
+  const { sub, email } = req.decoded;
   try {
-    let users = await db
-      .select()
-      .from("users")
-      .where({ email })
-      .andWhere({ sub });
+    let users = await db('users')
+                        .where({ email })
+                        .andWhere({ sub });
 
-    stripe.transfers.list({ limit: 10 }, async (err, transfers) => {
-      if (err) return res.status(500).json({ message: err });
-      const payout = transfers.data.filter(
-        payout => payout.destination === _customer.stripe_payout_id
-      );
+      stripe.transfers.list({ limit: 10 }, async (err, transfers) => {
+        if (err) {
+          return res.status(500).json({ err });
+        }
+        
+        const payout = transfers.data.filter(
+          payout => payout.destination === _customer.stripe_payout_id
+        );
+                    
+        const total_amount = payout.map(payout => payout.amount).reduce((a, b) => a + b) / 100;
 
-      const total_amount =
-        payout.map(payout => payout.amount).reduce((a, b) => a + b, 0) / 100;
+        if(users) {
+          const result = await users.map(async user => {
+          const offers = await db("offers")
+                                .where({ user_id: user.id });
+          const ads = await db("ads")
+                              .where({ user_id: user.id });
+          const agreements = await db.select('ag.*', 'o.id as test_id')
+                                    .from('agreements as ag')
+                                    .join('offers as o','o.id','ag.offer_id' )
+                                    .where({ affiliate_id: user.id });
 
-      if (users) {
-        const result = await users.map(async user => {
-          let offers = await db
-            .select()
-            .from("offers")
-            .where({ user_id: user.id });
-          const ads = await db
-            .select()
-            .from("ads")
-            .where({ user_id: user.id });
-          const agreements = await db
-            .select("ag.*", "o.id as test_id")
-            .from("agreements as ag")
-            .join("offers as o", "o.id", "ag.offer_id")
-            .where({ affiliate_id: user.id });
-
-          user.total_amount = total_amount;
+          user.stripe_balance = total_amount;                           
           user.offers = offers.length;
           user.ads = ads.length;
-          user.agreements = acct_type === "affiliate" ? agreements.length : 0;
+          user.agreements = agreements.length;
+
           return user;
-        });
+      });
 
-        Promise.all(result).then(completed => {
-          users = completed;
-          res.status(200).json(users[0]);
-        });
-      } else {
-        res.status(500).json({ message: "Users do not exist." });
-      }
-    });
-  } catch ({ message }) {
-    res.status(404).json({ message });
-  }
-});
-
-// @route    GET api/users
-// @desc     Get user by id
-// @Access   Private
-route.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await models.findBy("users", { id });
-    if (user) {
-      res.status(200).json(user);
+      Promise.all(result).then(completed => {
+        users = completed;
+        res.status(200).json(users[0]);
+      });
     } else {
-      res.status(500).json({ message: "User does not exist." });
+      res.status(500).json({ message: "Users do not exist." });
     }
-  } catch ({ message }) {
+  })
+  } catch({ message }) {
     res.status(404).json({ message });
   }
 });
@@ -97,27 +73,23 @@ route.put("/", authenticate, multipart, async (req, res) => {
   const { email, sub } = req.body;
 
   if (email || sub) {
-    return res
-      .status(500)
-      .json({ message: "updating email and sub is not allowed" });
+    return res.status(500).json({ message: "Updating email and sub is not allowed." });
   }
 
   if (!req.files.image_url) {
     const success = await models.update("users", id, { ...req.body });
     if (success) {
       const user = await models.findBy("users", { id });
-      res.status(200).json({ user, message: "User edited successfully." });
+      res.status(204).json({ user, message: "User edited successfully." });
     } else {
-      res
-        .status(404)
-        .json({ message: "There was an issue editing this user." });
+      res.status(404).json({ message: "There was an issue editing this user." });
     }
   } else {
     // ------------- cloudinary - ---------
     cloudinary.v2.uploader.upload(
       req.files.image_url.path,
       async (error, result) => {
-        if (error) return res.status(500).json({ message: error });
+        if (error) return res.status(500).json({ error });
         try {
           // ------------- update - ---------
           const success = await models.update("users", id, {
@@ -127,13 +99,9 @@ route.put("/", authenticate, multipart, async (req, res) => {
 
           if (success) {
             const user = await models.findBy("users", { id });
-            res
-              .status(200)
-              .json({ user, message: "User edited successfully." });
+            res.status(204).json({ user, message: "User edited successfully." });
           } else {
-            res
-              .status(404)
-              .json({ message: "There was an issue editing this user." });
+            res.status(404).json({ message: "There was an issue editing this user." });
           }
         } catch ({ message }) {
           res.status(500).json({ message });
@@ -151,11 +119,9 @@ route.delete("/", authenticate, async (req, res) => {
   try {
     const success = await models.remove("users", id);
     if (success) {
-      res.status(200).json({ message: "User deleted successfully." });
+      res.status(204).json({ message: "User deleted successfully." });
     } else {
-      res
-        .status(500)
-        .json({ message: "There was an issue deleting this user." });
+      res.status(500).json({ message: "There was an issue deleting this user." });
     }
   } catch ({ message }) {
     res.status(404).json({ message });
