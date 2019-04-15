f88b3d144ddd773dc75f08e63ff9548f3d599b42
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-15 18:44:22 -0400

    Ads only show when offer is active

--- common/helpers.js
+++ common/helpers.js
@@ -245,8 +245,10 @@ const agreementsByUserId = user =>
 const allAdsByAffiliateId = affiliate_id =>
   db("agreements as ag")
     .join("ads as ad", "ag.offer_id", "ad.offer_id")
+    .join("offers as o", "o.id", "ag.offer_id")
     .select("ad.*", "ag.id as agreement_id")
-    .where("affiliate_id", affiliate_id);
+    .where("affiliate_id", affiliate_id)
+    .andWhere("status", true);
 
 const offerAgreementsAffiliates = (user_id, allOffer) =>
   db
