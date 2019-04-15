6b749010d5b832129001737ee3d2f0a7b9b194a8
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-15 12:19:33 -0400

    cleaned up auth route

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,40 +1,30 @@
 const route = require("express").Router();
 const models = require("../../common/helpers");
-const db = require("../../data/dbConfig");
 const errorHelper = require("../../error-helper/errorHelper");
 
 // @route    /api/auth/register
 // @desc     POST signing up user
 // @Access   Public
 route.post("/register", async (req, res) => {
-  const {
-    name,
-    email,
-    image_url,
-    nickname,
-    sub,
-  } = req.body;
+  const { name, email, image_url, nickname, sub } = req.body;
 
   if (!name || !email || !image_url || !nickname || !sub) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
   try {
-    const exists = await db('users')
-                           .where({ email })
-                           .andWhere({ sub })
-                           .first()
+    const exists = await models.findBy("users", { email, sub });
 
     if (exists) {
       return res.status(200).json(exists);
     }
 
-    const [ id ] = await models.add("users", req.body);
-    
+    const [id] = await models.add("users", req.body);
+
     if (id) {
       const user = await models.findBy("users", { id });
       return res.status(200).json(user);
-    } 
+    }
   } catch (error) {
     return errorHelper(500, error, res);
   }
